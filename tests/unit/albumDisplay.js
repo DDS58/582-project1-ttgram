f6d1a9fc314637c3823416e81956d0b06d148883
David <2295458@vaniercollege.qc.ca>
2023-08-08 09:32:01 -0400

    trying to divide test

---
+++ tests/unit/albumDisplay.js
@@ -0,0 +1,19 @@
+// import { shallowMount } from "@vue/test-utils";
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+// describe("HelloWorld.vue", () => {
+//   it("renders props.msg when passed", () => {
+//     const msg = "new message";
+//     const wrapper = shallowMount(HelloWorld, {
+//       props: { msg },
+//     });
+//     expect(wrapper.text()).toMatch(msg);
+//   });
+// });
+
+it("opens the user profile when clicking on the username", async () => {});
+it("change page to show full album", async () => {});
+it("opens a model with an enlarged photo when clicked", async () => {});
+it("opens a comment-user profile when username is clicked", async () => {});
+it('emits a "follow" event when clicking the "Follow" button', async () => {});
+it('emits an "unfollow" event when clicking the "Unfollow" button', async () => {});
